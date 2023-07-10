16b9bcdf06580300b4b4c16d912f1d9fa041146d
Saad Ahmad <saad.ahmad1024@gmail.com>
2023-07-11 04:54:23 +0500

    dkfjdskfjdslkfsdfd

---
+++ Lab Tasks/1/myFile.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    printf("Saad Ahmad aka ayyzenn\n");
+    system("mkdir test");
+    system("ls");
+    system("rm test -r");
+    return 0;
+}
